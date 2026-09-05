174f0a9310b0a0e67f131005919f2b36e10ecc38
Frug <kokarev1001@mail.ru>
2026-09-05 17:18:03 +0300

    get and post db swith on a db_test_database

--- src/tests/core/01-get.test.js
+++ src/tests/core/01-get.test.js
@@ -1,60 +1,83 @@
-import test, { describe, it } from "node:test";
+import test, { after, before, describe } from "node:test";
 import { strict as assert } from "node:assert";
 import req from "supertest";
 import { ErrorModule } from "../../err/ErrorModule.js";
 import app from "../../app.js";
+import { dbTableTruncateAndCreateSeedQuest, dbTableQuestInit } from "../../analytics/dbInit.js";
+import pool from "../../db/pool.js";
 
-test("GET /quests returns an array", async () => {
-    const res = await req(app).get("/quests");
-    
-    assert.equal(res.status, 200);
-    assert.ok(Array.isArray(res.body));
+pool.options.database = process.env.DB_TEST_DATABASE;
+
+after(() => pool.end());
+
+test("db is a test database", async () => {
+    try {
+        const res = await pool.query("SELECT current_database() AS db_name");
+        assert.equal(res.rows[0].db_name, process.env.DB_TEST_DATABASE);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 });
 
-test("GET /quests/1 returns an object", async () => {
-    const res = await req(app).get("/quests/1");
-    
-    assert.equal(res.status, 200);
-    assert.ok(res.body);
-});
-
-test("GET /quests/abc and /quests/0 return 400 + INVALID_QUEST_ID", async () => {
-    const resString = await req(app).get("/quests/abc");
-    const resNumber = await req(app).get("/quests/0");
-    
-    assert.equal(resString.status, 400);
-    assert.equal(resNumber.status, 400);
-
-    assert.equal(resString.body.error.code, ErrorModule.errCodesText.invalidQuestIdText);
-    assert.equal(resNumber.body.error.code, ErrorModule.errCodesText.invalidQuestIdText);
-});
-
-test("GET /quests/999 return 404 + QUEST_NOT_FOUND", async () => {
-    const res = await req(app).get("/quests/999");
-
-    assert.equal(res.status, 404);
-    assert.equal(res.body.error.code, ErrorModule.errCodesText.questNotFoundText);
-});
-
-test("GET /unknown return 404 + ROUTE_NOT_FOUND", async () => {
-    const res = await req(app).get("/unknown");
-
-    assert.equal(res.status, 404);
-    assert.equal(res.body.error.code, ErrorModule.errCodesText.routeNotFoundText);
-});
-
-describe("GET /quests?difficulty", () => {
-    it("200 - valid (easy)", async () => {
-        const res = await req(app).get("/quests?difficulty=easy");
-        
-        assert.equal(res.status, 200);
-        assert.ok(res.body.every((item) => item.difficulty === "easy"));
+describe("GET", () => {
+    before(async () => {
+        await dbTableQuestInit(pool);
+        await dbTableTruncateAndCreateSeedQuest(pool);
     });
 
-    it("400 + VALIDATION_ERROR - unknown", async () => {
-        const res = await req(app).get("/quests?difficulty=unknown");
-
-        assert.equal(res.status, 400);
-        assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+    test("/quests returns an array", async () => {
+        const res = await req(app).get("/quests");
+        
+        assert.equal(res.status, 200);
+        assert.ok(Array.isArray(res.body));
+    });
+    
+    test("/quests/1 returns an object", async () => {
+        const res = await req(app).get("/quests/1");
+        
+        assert.equal(res.status, 200);
+        assert.ok(res.body);
+    });
+    
+    test("/quests/abc and /quests/0 return 400 + INVALID_QUEST_ID", async () => {
+        const resString = await req(app).get("/quests/abc");
+        const resNumber = await req(app).get("/quests/0");
+        
+        assert.equal(resString.status, 400);
+        assert.equal(resNumber.status, 400);
+    
+        assert.equal(resString.body.error.code, ErrorModule.errCodesText.invalidQuestIdText);
+        assert.equal(resNumber.body.error.code, ErrorModule.errCodesText.invalidQuestIdText);
+    });
+    
+    test("/quests/999 return 404 + QUEST_NOT_FOUND", async () => {
+        const res = await req(app).get("/quests/999");
+    
+        assert.equal(res.status, 404);
+        assert.equal(res.body.error.code, ErrorModule.errCodesText.questNotFoundText);
+    });
+    
+    test("/unknown return 404 + ROUTE_NOT_FOUND", async () => {
+        const res = await req(app).get("/unknown");
+    
+        assert.equal(res.status, 404);
+        assert.equal(res.body.error.code, ErrorModule.errCodesText.routeNotFoundText);
+    });
+    
+    describe("/quests?difficulty", () => {
+        test("200 - valid (easy)", async () => {
+            const res = await req(app).get("/quests?difficulty=easy");
+            
+            assert.equal(res.status, 200);
+            assert.ok(res.body.every((item) => item.difficulty === "easy"));
+        });
+    
+        test("400 + VALIDATION_ERROR - unknown", async () => {
+            const res = await req(app).get("/quests?difficulty=unknown");
+    
+            assert.equal(res.status, 400);
+            assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+        });
     });
 });
--- src/tests/core/02-post.test.js
+++ src/tests/core/02-post.test.js
@@ -1,88 +1,101 @@
-import test, { afterEach, beforeEach, describe, it } from "node:test";
+import test, { before, afterEach, after, describe } from "node:test";
 import { strict as assert } from "node:assert";
 import req from "supertest";
 import { postQuestFixtures } from "../fixtures/post/postStorage.js";
 import { ErrorModule } from "../../err/ErrorModule.js";
-import path from "node:path";
-import fs from "node:fs";
+import pool from "../../db/pool.js";
+import { dbTableTruncateAndCreateSeedQuest, dbTableQuestInit, dbTruncateTableQuest } from "../../analytics/dbInit.js";
+import app from "../../app.js";
 
-process.env.DATA_FILE_PATH = path.join(process.cwd(), "src", "tests", "tmp", "quests.json");
+pool.options.database = process.env.DB_TEST_DATABASE;
 
-const {default: app} = await import("../../app.js");
+after(() => pool.end());  
 
-beforeEach(() => {
-    if (fs.statSync(process.env.DATA_FILE_PATH).size > 0) {
-        fs.truncateSync(process.env.DATA_FILE_PATH, 0);
-    } 
-})
-
-afterEach(() => {
-    fs.truncateSync(process.env.DATA_FILE_PATH, 0);
+test("db is a test database", async () => {
+    try {
+        const res = await pool.query("SELECT current_database() AS db_name");
+        assert.equal(res.rows[0].db_name, process.env.DB_TEST_DATABASE);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 });
 
-test("POST /quests 201 + create quest with id/createdAt/completed/ description by default", async () => {
-    const res = await req(app).post("/quests").send(postQuestFixtures.valid);
-    
-    assert.equal(res.status, 201);
-    assert.ok(Object.hasOwn(res.body, "id"));
-    assert.ok(Object.hasOwn(res.body, "createdAt"));
-    assert.ok(Object.hasOwn(res.body, "completed"));
-    assert.ok(Object.hasOwn(res.body, "description"));
-});
-
-test("POST /quests 400 + VALIDATION_ERROR - title missing", async () => {
-    const res = await req(app).post("/quests").send(postQuestFixtures.invalid.titleMissing);
-    
-    assert.equal(res.status, 400);
-
-    assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
-    
-    assert.ok(res.body.error.details.title);
-});
-
-test("POST /quests 400 + VALIDATION_ERROR - rewardXp string", async () => {
-    const res = await req(app).post("/quests").send(postQuestFixtures.invalid.rewardXpString);
-
-    assert.equal(res.status, 400);
-
-    assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
-});
-
-test("POST /quests 400 + VALIDATION_ERROR - rewardXp=0, decimal, wrong difficulty", async () => {
-    const res1 = await req(app).post("/quests").send(postQuestFixtures.invalid.rewardXpZeroDifficuluty);
-    const res2 = await req(app).post("/quests").send(postQuestFixtures.invalid.rewardXpDecimal);
-
-    assert.equal(res1.status, 400);
-    assert.equal(res2.status, 400);
-
-    assert.equal(res1.body.error.code, ErrorModule.errCodesText.validErrorText);
-    assert.equal(res2.body.error.code, ErrorModule.errCodesText.validErrorText);
-
-    assert.ok(Object.hasOwn(res1.body.error.details, "difficulty"));
-    assert.ok(Object.hasOwn(res1.body.error.details, "rewardXp"));
-    assert.ok(Object.hasOwn(res2.body.error.details, "rewardXp"));
-});
-
-test("POST /quests 400 + VALIDATION_ERROR - completed/id/createdAt/unknownField", async () => {
-    const res = await req(app).post("/quests").send(postQuestFixtures.invalid.completedIdCreatedAtUnknownField);
-
-    assert.equal(res.status, 400);
-
-    assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
-});
-
-describe("POST /quests - description", () => {
-    it("400 + VALIDATION_ERROR - is not a string", async () => {
-        const res = await req(app).post("/quests").send(postQuestFixtures.invalid.descriptionType);
+describe("POST", () => {
+    before(async () => {
+        await dbTableQuestInit(pool);
+        await dbTableTruncateAndCreateSeedQuest(pool);
+    });
         
-        assert.equal(res.status, 400);
-        assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+    afterEach(async () => {
+        await dbTableTruncateAndCreateSeedQuest(pool);
     });
 
-    it("400 + VALIDATION_ERROR - length > 300", async () => {
-        const res = await req(app).post("/quests").send(postQuestFixtures.invalid.descriptionLength);
+    test("/quests 201 + create quest with id/createdAt/completed/ description by default", async () => {
+        const res = await req(app).post("/quests").send(postQuestFixtures.valid);
+        
+        assert.equal(res.status, 201);
+        assert.ok(Object.hasOwn(res.body, "id"));
+        assert.ok(Object.hasOwn(res.body, "createdAt"));
+        assert.ok(Object.hasOwn(res.body, "completed"));
+        assert.ok(Object.hasOwn(res.body, "description"));
+    });
+    
+    test("/quests 400 + VALIDATION_ERROR - title missing", async () => {
+        const res = await req(app).post("/quests").send(postQuestFixtures.invalid.titleMissing);
         
         assert.equal(res.status, 400);
+    
+        assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+        
+        assert.ok(res.body.error.details.title);
+    });
+    
+    test("/quests 400 + VALIDATION_ERROR - rewardXp string", async () => {
+        const res = await req(app).post("/quests").send(postQuestFixtures.invalid.rewardXpString);
+    
+        assert.equal(res.status, 400);
+    
         assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
     });
+    
+    test("/quests 400 + VALIDATION_ERROR - rewardXp=0, decimal, wrong difficulty", async () => {
+        const res1 = await req(app).post("/quests").send(postQuestFixtures.invalid.rewardXpZeroDifficuluty);
+        const res2 = await req(app).post("/quests").send(postQuestFixtures.invalid.rewardXpDecimal);
+    
+        assert.equal(res1.status, 400);
+        assert.equal(res2.status, 400);
+    
+        assert.equal(res1.body.error.code, ErrorModule.errCodesText.validErrorText);
+        assert.equal(res2.body.error.code, ErrorModule.errCodesText.validErrorText);
+    
+        assert.ok(Object.hasOwn(res1.body.error.details, "difficulty"));
+        assert.ok(Object.hasOwn(res1.body.error.details, "rewardXp"));
+        assert.ok(Object.hasOwn(res2.body.error.details, "rewardXp"));
+    });
+    
+    test("/quests 400 + VALIDATION_ERROR - completed/id/createdAt/unknownField", async () => {
+        const res = await req(app).post("/quests").send(postQuestFixtures.invalid.completedIdCreatedAtUnknownField);
+    
+        assert.equal(res.status, 400);
+    
+        assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+    });
+    
+    describe("/quests - description", () => {
+        test("400 + VALIDATION_ERROR - is not a string", async () => {
+            const res = await req(app).post("/quests").send(postQuestFixtures.invalid.descriptionType);
+            
+            assert.equal(res.status, 400);
+            assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+        });
+    
+        test("400 + VALIDATION_ERROR - length > 300", async () => {
+            const res = await req(app).post("/quests").send(postQuestFixtures.invalid.descriptionLength);
+            
+            assert.equal(res.status, 400);
+            assert.equal(res.body.error.code, ErrorModule.errCodesText.validErrorText);
+        });
+    });
 });
+
